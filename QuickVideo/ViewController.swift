40660648dbdd46ad696294ce8691e6cd0b35531f
GTCC <gtcc@gtccs-macbook-pro.local>
2015-10-28 16:30:13 +0800

    init

--- QuickVideo/ViewController.swift
+++ QuickVideo/ViewController.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UISearchBarDelegate {
 
+    @IBOutlet weak var searchBar: UISearchBar!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +21,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        print(searchBar.text)
+        searchBar.resignFirstResponder()
+        
+        let controller = storyboard?.instantiateViewControllerWithIdentifier("VideoDetail") as! VideoDetail
+        controller.searchContent = searchBar.text!
+        self.navigationController?.pushViewController(controller, animated: true)
+    }
 }
 
